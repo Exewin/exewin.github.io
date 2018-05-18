08216db86539842e0916d9e41e66942571a34bc0
exewin <windowsmemphis@gmail.com>
2018-05-18 22:49:42 +0200

    basics
    project.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>EXEWIN</title>
+<meta charset="UTF-8"/>
+<link rel="Stylesheet" href="style.css">
+</head>
+<body>
+<?php
+include 'header.html';
+?>
+<div class="mainBlock">
+
+<img src="comp.jpg" width="500px"></img>
+
+<br/>
+
+<a href="projects.php" class="button">Projects</a>
+<a href="#" class="button">Skills</a>
+<a href="#" class="button">Contact</a>
+
+</div>
+<?php
+include 'footer.html';
+?>
+
+
+
+</body>
+</html>
